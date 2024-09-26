cbe5cc0cc538d1fddf6ecc8f7e6314aee79452f7
Lumine2024 <149254783+Lumine2024@users.noreply.github.com>
2024-09-26 09:45:09 +0800

    Update main.c

--- level1/p03_all_primes/main.c
+++ level1/p03_all_primes/main.c
@@ -1,6 +1,23 @@
-#include <printf.h>
-
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<time.h>
+#include<math.h>
+bool isprime(unsigned long long n) {
+	int a = sqrt(n);
+	for(int i = 2; i <= a; ++i) {
+		if(n % i == 0) return false;
+	}
+	return true;
+}
 int main() {
-    printf("hello world!\n");
-    return 0;
+	clock_t tprev = clock();
+	for(int i = 2; i < 1000; ++i) {
+		if(isprime(i)) {
+			printf("%d\n", i);
+		}
+	}
+	clock_t tnow = clock();
+	printf("消耗了%ld毫秒", tnow - tprev);
+	return 0;
 }
