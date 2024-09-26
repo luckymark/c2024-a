ede2fa7fc06ff2d5a71340ebe205547479186214
Lumine2024 <149254783+Lumine2024@users.noreply.github.com>
2024-09-26 19:04:55 +0800

    Update main.c

--- level1/p05_encrypt_decrypt/main.c
+++ level1/p05_encrypt_decrypt/main.c
@@ -1,6 +1,48 @@
-#include <printf.h>
-
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<math.h>
+//free res after using it
+char *encrypt(char *ori, int id) {
+	int len = strlen(ori);
+	char *res = (char *) malloc(sizeof(char) * len + 1);
+	if(len % 2 == 1) {
+		for(int i = 0; i < len; ++i) {
+			res[i] = ori[(2 * i >= len ? 2 * i - len : 2 * i)] + id;
+		}
+	}
+	else {
+		for(int i = 0; i < len; ++i) {
+			res[i] = ori[(2 * i >= len ? 2 * i - len + 1 : 2 * i)] + id;
+		}
+	}
+	res[len] = '\0';
+	return res;
+}
+//same,free res
+char *decrypt(char *code, int id) {
+	int len = strlen(code);
+	char *res = (char *) malloc(sizeof(char) * len + 1);
+	if(len % 2 == 1) {
+		for(int i = 0; i < len; ++i) {
+			res[(2 * i >= len ? 2 * i - len : 2 * i)] = code[i] - id;
+		}
+	}
+	else {
+		for(int i = 0; i < len; ++i) {
+			res[(2 * i >= len ? 2 * i - len + 1 : 2 * i)] = code[i] - id;
+		}
+	}
+	res[len] = '\0';
+	return res;
+}
 int main() {
-    printf("hello world!\n");
-    return 0;
+	char *res = "hello world";
+	printf("%s\n", res);
+	res = encrypt(res, 16);
+	printf("%s\n", res);
+	res = decrypt(res, 16);
+	printf("%s\n", res);
+	free(res);
+	return 0;
 }
