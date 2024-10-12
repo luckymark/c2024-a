23c72833a25a3bd0343a89807397222d781f0ed5
Lumine2024 <149254783+Lumine2024@users.noreply.github.com>
2024-10-12 20:47:00 +0800

    Update main.c

--- level1/Ex1_Queue/main.c
+++ level1/Ex1_Queue/main.c
@@ -1,60 +1,87 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdbool.h>
-void *nullptr = (void *) 0;
-struct Queue {
-	int *val;
-	size_t size;
-	size_t capacity;
+#define nullptr (void*)0
+struct _queueNode {
+	int val;
+	struct _queueNode *next;
 };
-typedef struct Queue Queue;
-Queue *initialize() {
+typedef struct _queueNode Queue;
+Queue *Init_with_val(int val) {
 	Queue *q = (Queue *) malloc(sizeof(Queue));
-	q->capacity = 0;
-	q->size = 0;
-	q->val = nullptr;
-	return q;
-}
-Queue *Initialize_with_capacity(size_t capacity) {
-	Queue *q = initialize();
-	q->capacity = capacity;
-	q->size = 0;
-	q->val = (int *) malloc(sizeof(int) * q->capacity);
-	return q;
-}
-Queue *Initialize_with_array(int *arr, size_t arrsize, size_t capacity) {
-	Queue *q = initialize();
-	q->capacity = capacity;
-	q->size = arrsize;
-	q->val = (int *) malloc(sizeof(int) * q->capacity);
-	for(size_t i = 0; i < arrsize; i++) {
-		q->val[i] = arr[i];
+	if(!q) {
+		puts("Memory allocation failed\n");
+		exit(1);
 	}
+	q->val = val;
+	q->next = nullptr;
 	return q;
 }
-void append(Queue *q, int val) {
-	if(q->size <= q->capacity) {
-		q->val[q->size++] = val;
+Queue *Init_with_next(int val, Queue *next) {
+	Queue *q = (Queue *) malloc(sizeof(Queue));
+	if(!q) {
+		puts("Memory allocation failed\n");
+		exit(1);
 	}
+	q->val = val;
+	q->next = next;
+	return q;
 }
-void pop(Queue *q) {
-	--q->size;
+Queue *Init_by_array(int *arr, int n) {
+	Queue *head = nullptr;
+	for(int i = n - 1; i >= 0; i--) {
+		head = Init_with_next(arr[i], head);
+	}
+	return head;
 }
-bool isempty(Queue *q) {
-	return q->size == 0;
+void Add_to_end(Queue *head1, Queue *head2) {
+	if(head1 == nullptr) return;
+	Queue *temp = head1;
+	while(temp->next != nullptr) {
+		temp = temp->next;
+	}
+	temp->next = head2;
 }
-bool isfull(Queue *q) {
-	return q->size == q->capacity;
+void push(Queue *head, int val) { // 给我气炸了，push传一维指针可以，pop非要传二维指针
+	Queue *temp = head;
+	while(temp->next != nullptr) {
+		temp = temp->next;
+	}
+	temp->next = Init_with_val(val);
 }
-int get(Queue *q) {
-	return q->val[q->size - 1];
+int pop(Queue **head) {
+	if((*head)->next == nullptr) {
+		puts("Queue is empty\n");
+		exit(1);
+	}
+	int val = (*head)->val;
+	Queue *temp = *head;
+	*head = (*head)->next;
+	free(temp);
+	return val;
+}
+int top(Queue *head) {
+	if(head == nullptr) {
+		puts("Queue is empty\n");
+		exit(1);
+	}
+	return head->val;
 }
 int main() {
-	int val[5] = { 1,2,3,4,5 };
-	Queue *q = Initialize_with_array(val, 5, 10);
-	append(q, 6);
-	printf("%d\n", get(q));
-	pop(q);
-	printf("%d\n", get(q));
+	int arr[] = { 1, 2, 3, 4, 5 };
+	Queue *q1 = Init_by_array(arr, 5);
+	printf("%d", top(q1));
+	push(q1, 6);
+	printf("\n%d", top(q1));
+	pop(&q1);
+	printf("\n%d", top(q1));
+	pop(&q1);
+	printf("\n%d", top(q1));
+	pop(&q1);
+	printf("\n%d", top(q1));
+	pop(&q1);
+	printf("\n%d", top(q1));
+	pop(&q1);
+	printf("\n%d", top(q1));
 	return 0;
 }
